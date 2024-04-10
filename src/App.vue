34b93022b89e0553ee30867fde56bb6a95e98d6d
Dinobergue Viana <dinbergueviana@MacBook-Air-de-Dinbergue.local>
2024-04-10 00:38:17 -0300

    removes default assets and components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Site em construção...</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
