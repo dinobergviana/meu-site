15514762e2e012206f3aa4eb689f312259eb63db
dinobergviana <dinobergueviana@gmail.com>
2024-04-22 20:34:46 -0300

    adds hero

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,17 +3,6 @@
     <div class="footer-container">
       <span>&#169; 2024 Dinobergue Viana</span>
     </div>
-    <div class="medias-container">
-      <a href="https://www.linkedin.com/in/dinobergue-viana-28574218b/" target="blank" class="media-link">
-        <img src="../assets/linkedin.svg" alt="">
-      </a>
-      <a href="https://github.com/dinobergviana" target="blank" class="media-link">
-        <img src="../assets/github.svg" alt="">
-      </a>
-      <a href="https://api.whatsapp.com/send?phone=5586998055574" target="blank" class="media-link">
-        <img src="../assets/whatsapp.svg" alt="">
-      </a>
-    </div>
   </footer>
 </template>
 
@@ -27,26 +16,5 @@ export default {
   padding: 1em;
   text-align: center;
   color: var(--gray-400);
-}
-
-.medias-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.6em;
-  padding: 1em;
-}
-
-.media-link {
-  text-decoration: none;
-  opacity: 0.7;
-}
-
-.media-link:hover {
-  opacity: 1;
-}
-
-.media-link img {
-  height: 20px;
 }
 </style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <section class="home-container">
     <div>
-      <h1 class="title">Site em construção...</h1>
+      <h1 class="title">Olá, eu sou Dinobergue Viana.</h1>
     </div>
+    <div class="medias-container">
+      <a href="https://www.linkedin.com/in/dinobergue-viana-28574218b/" target="blank" class="media-link">
+        <img src="../../assets/linkedin.svg" alt="">
+      </a>
+      <a href="https://github.com/dinobergviana" target="blank" class="media-link">
+        <img src="../../assets/github.svg" alt="">
+      </a>
+      <a href="https://api.whatsapp.com/send?phone=5586998055574" target="blank" class="media-link">
+        <img src="../../assets/whatsapp.svg" alt="">
+      </a>
+    </div>
+    <span class="description">Site em construção.</span>
   </section>
 </template>
 <script>
@@ -18,10 +30,43 @@ export default {
   padding: 2em;
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
 }
 
+.home-container div {
+  text-align: center;
+}
+
 .title {
-  font-weight: 500;
+  font-weight: 700;
+  font-size: 3em;
+  margin-bottom: 0.2em;
+}
+
+.description {
+  font-size: 1.2em;
+  color: var(--red-200);
+}
+
+.medias-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.6em;
+  padding: 1em;
+}
+
+.media-link {
+  text-decoration: none;
+  opacity: 0.7;
+}
+
+.media-link:hover {
+  opacity: 1;
+}
+
+.media-link img {
+  height: 20px;
 }
 </style>
