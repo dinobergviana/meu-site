3329ff865496a0f87df2b89baffdd00e0b3a14ef
dinobergviana <dinobergueviana@gmail.com>
2024-09-16 00:09:44 -0300

    feat: add about translation file

---
+++ src/components/about/about-lang.js
@@ -0,0 +1,16 @@
+export const ABOUT_LANG = {
+  pt: {
+    title: 'sobre mim.',
+    p1: 'Entrei no mercado de programação como <strong>Desenvolvedor Web</strong> em 2021 após dicidir mudar de carreira no ano anterior.',
+    p2: 'Atualmente <strong>estou cursando Análise e Desenvolvimento de Sistemas</strong> pela Uni Dom Bosco e <strong>possuo certificado em Desenvolvimento Web Full Stack Node</strong> pela <strong><a href="https://www.digitalhouse.com/br" target="_blank">Digital House</a></strong>.',
+    p3: 'Além da faculdade, <strong>também sou aluno do <a href="http://curso.dev" target="_blank">curso.dev</a></strong> do <strong><a href="https://filipedeschamps.com.br/" target="_blank">Filipe Deschamps</a></strong>.',
+    small: 'Em breve contarei com mais detalhes como foi a minha transição de carreira.'
+  },
+  en: {
+    title: 'about me.',
+    p1: 'I entered the programming market as a <strong>Web Developer</strong> in 2021 after deciding to change careers the previous year.',
+    p2: 'I am currently <strong>studying Systems Analysis and Development</strong> at Uni Dom Bosco and <strong>hold a Full Stack Node Web Development</strong> certificate from <strong><a href="https://www.digitalhouse.com/br" target="_blank">Digital House</a></strong>.',
+    p3: 'In addition to college, <strong>I am also a student of</strong> <strong><a class="link" href="https://filipedeschamps.com.br/" target="_blank">Filipe Deschamps</a></strong> on <strong><a href="http://curso.dev" target="_blank">curso.dev</a></strong>.',
+    small: 'Soon, I will share more details about my career transition.'
+  },
+}
