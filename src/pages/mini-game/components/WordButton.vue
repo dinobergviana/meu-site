9f5cdb5cf1f9cfbe0186f58b8494f76ae3fcda39
dinobergviana <dinobergueviana@gmail.com>
2025-05-25 01:01:41 -0300

    feat: add word-button select functionality

---
+++ src/pages/mini-game/components/WordButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    class="word-button"
+    type="button"
+    :class="{
+      'disabled': word.disabled,
+      'success': word.matched,
+      'error': word.error,
+      'selected': word.selected
+    }"
+    :disabled="word.disabled"
+    @click="emitPressed(word)"
+  >
+    {{ word.label }}
+  </button>
+</template>
+
+<script setup>
+  defineProps({
+    word: {
+      type: Object,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(['pressed'])
+
+  function emitPressed(word) {
+    emit('pressed', word)
+  }
+</script>
+
+<style scoped>
+  .word-button {
+    height: 80px;
+    width: 200px;
+    border: 0;
+    font-size: 1.5rem;
+    color: var(--gray-300);
+    border-radius: 8px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px, rgba(0, 0, 0, 0) 0px 7px 13px -3px, rgba(0, 0, 0, 0.1) 0px -4px 0px inset;
+  }
+
+  .word-button:active {
+    transform: translateY(2px);
+  }
+
+  .word-button + .word-button  {
+    margin-top: 20px;
+  }
+
+  .success {
+    border: 1px solid var(--green-200);
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px, rgba(0, 0, 0, 0) 0px 7px 13px -3px, var(--green-200) 0px -4px 0px inset;
+  }
+
+  .selected {
+    border: 1px solid var(--blue-200);
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px, rgba(0, 0, 0, 0) 0px 7px 13px -3px, var(--blue-200) 0px -4px 0px inset;
+  }
+  
+  .error {
+    border: 1px solid var(--red-100);
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px, rgba(0, 0, 0, 0) 0px 7px 13px -3px, var(--red-100) 0px -4px 0px inset;
+  }
+  
+  .disabled {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  @media (max-width: 425px) {
+    .word-button {
+      width: 150px;
+    }
+  }
+</style>
--- src/pages/mini-game/index.vue
+++ src/pages/mini-game/index.vue
@@ -2,23 +2,116 @@
   <div class="container">
     <div class="game-wrapper">
       <div class="column">
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
+        <WordButton v-for="word in leftWords" :word="word" :key="`left-${word.id}`" @pressed="handleButtonPressed" />
       </div>
       <div class="column">
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
-        <button class="word-button" type="button">teste</button>
+        <WordButton v-for="word in rightWords" :word="word" :key="`right-${word.id}`" @pressed="handleButtonPressed" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+  import { ref } from "vue"
+import WordButton from "./components/WordButton.vue"
 
+  const leftWords = ref([
+    {
+      id: 1,
+      label: 'fruta',
+      lang_id: 1,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'pt'
+    },
+    {
+      id: 2,
+      label: 'pão',
+      lang_id: 2,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'pt'
+    },
+    {
+      id: 3,
+      label: 'aula',
+      lang_id: 3,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'pt'
+    },
+    {
+      id: 4,
+      label: 'corda',
+      lang_id: 4,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled:false,
+      lang: 'pt'
+    }
+  ])
+
+  const rightWords = ref([
+    {
+      id: 2,
+      label: 'bread',
+      lang_id: 2,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled:false,
+      lang: 'en'
+    },
+    {
+      id: 1,
+      label: 'fruit',
+      lang_id: 1,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'en'
+    },
+    {
+      id: 4,
+      label: 'string',
+      lang_id: 4,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'en'
+    },
+    {
+      id: 3,
+      label: 'class',
+      lang_id: 3,
+      matched: false,
+      error: false,
+      selected: false,
+      used: false,
+      disabled: false,
+      lang: 'en'
+    },
+  ])
+
+  function handleButtonPressed(word) {
+    word.selected = !word.selected
+  }
 </script>
 
 <style scoped>
@@ -44,27 +137,9 @@
     justify-content: space-around;
   }
 
-  .word-button {
-    height: 80px;
-    width: 200px;
-    border: 0;
-    font-size: 1.5rem;
-    color: var(--gray-400);
-    border-radius: 8px;
-    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0) 0px 7px 13px -3px, rgba(0, 0, 0, 0.1) 0px -4px 0px inset;
-  }
-
-  .word-button + .word-button  {
-    margin-top: 20px;
-  }
-
   @media (max-width: 425px) {
     .game-wrapper {
       width: 100%;
-    }
-
-    .word-button {
-      width: 150px;
     }
   }
 </style>
