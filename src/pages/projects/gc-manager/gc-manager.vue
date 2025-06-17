4de775a1c6911fdc3e2c264e9679cfc571d4cc3a
dinobergviana <dinobergueviana@gmail.com>
2025-06-16 23:03:57 -0300

    feat: add backend extra infos

--- src/pages/projects/gc-manager/gc-manager.vue
+++ src/pages/projects/gc-manager/gc-manager.vue
@@ -177,27 +177,12 @@
             </p>
           </li>
         </ul>
-
-        <h3 class="tecnical-information-title">
-          📈 {{ t("projects.gcManager.articleSixSubtitleSeven") }}
-        </h3>
-        <ul class="tecnical-information-list-content">
-          <li>
-            <p>{{ t("projects.gcManager.articleSixListSevenItemOne") }}</p>
-          </li>
-          <li>
-            <p>{{ t("projects.gcManager.articleSixListSevenItemTwo") }}</p>
-          </li>
-          <li>
-            <p>{{ t("projects.gcManager.articleSixListSevenItemTree") }}</p>
-          </li>
-        </ul>
       </article>
 
       <article>
         <h2>{{ t("projects.gcManager.articleSevenTitle") }}</h2>
 
-        <h3 class="tecnical-information-title">Frontend</h3>
+        <h3 class="tecnical-information-title">Frontend - {{ t("projects.gcManager.userInterfaceInfo") }}</h3>
         <ul class="tecnical-information-list-content">
           <li>
             <p>React.</p>
@@ -210,12 +195,28 @@
         <h3 class="tecnical-information-title">Backend</h3>
         <ul class="tecnical-information-list-content">
           <li>
+            <p>
+              {{ t("projects.gcManager.hosting") }}: 
+              <a
+                href="https://vercel.com/"
+                target="_blank"
+              >
+                Vercel;
+              </a
+              >              
+            </p>
+          </li>
+          <li>
             <p>Node, Next, Docker, Postgres.</p>
           </li>
           <li>
             <p>Jest.</p>
           </li>
         </ul>
+
+        <p>
+          {{ t('projects.gcManager.backendFeatures') }}
+        </p>
       </article>
     </div>
   </section>
@@ -280,5 +281,13 @@ p {
 
 article + article {
   margin-top: 2rem;
+}
+
+.code-line {
+  background-color: var(--gray-100);
+  padding: 0.2rem 0.4rem;
+  border-radius: 4px;
+  font-family: monospace;
+  color: var(--gray-800);
 }
 </style>
